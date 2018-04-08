9b6afb0506fb079805080b4377d6d6935f71d6e9
jbdamask <jbdamask@gmail.com>
2018-04-08 12:52:02 -0400

    Changed lots. Found I'm not the only one with the problem. See (this)[https://github.com/IanHarvey/bluepy/issues/159] thread and associated GitHub example (code)[https://github.com/PrzemoF/Open-Cycling-Computer/blob/master/code/src/ble_sc.py]. I still don't have it right. I think what's going on is that I have two threads acting on the same Peripheral and this causes "Unexpected response" errors from BluePy.btle._waitResp

--- multicast_ble.py
+++ multicast_ble.py
@@ -8,8 +8,6 @@ def DBG(*args):
 
 class MyDelegate(DefaultDelegate):
 
-    txUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
-
     def __init__(self, addr):
         DefaultDelegate.__init__(self)
 	self.id = addr
@@ -18,55 +16,68 @@ class MyDelegate(DefaultDelegate):
     def handleNotification(self, cHandle, data):
 	DBG("Received notification from: ", self.id, cHandle, " send data ", binascii.b2a_hex(data))
 	self.d = data
-        map(self.broadcast, peripherals.values())
 
-    def broadcast(self, p):
-    	if(self.id == p.addr):
-            return
-	with lock:
-	    txh = peripherals[p.addr].getCharacteristics(uuid=self.txUUID)[0]
-        txh.write(self.d, True)
-
-class BleThread(threading.Thread):
+class BleThread(Peripheral, threading.Thread):
     
+    ## @var WAIT_TIME
+    # Time of waiting for notifications in seconds
+    WAIT_TIME = 0.3
+    ## @var EXCEPTION_WAIT_TIME
+    # Time of waiting after an exception has been raiesed or connection lost
+    EXCEPTION_WAIT_TIME = 10
+    txUUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+
     def __init__(self, peripheral_addr):
         threading.Thread.__init__(self)
-        self.peripheral_addr = peripheral_addr
+	Peripheral.__init__(self, peripheral_addr, addrType = "random")
+	self.delegate = MyDelegate(peripheral_addr)
+	self.withDelegate(self.delegate)
+	self.connected = True
+	self.featherState = ""
+        print " Configuring RX to notify me on change"
+        try:
+	    self.writeCharacteristic(35, b"\x01\x00", withResponse=True)
+	except BaseException:
+	    print "BaseException caught when subscribing to notifications for:  " + self.addr
+            print BaseException.message
+	    raise
 
     def run(self):
+	print "Starting Thread " + self.addr
+        while self.connected:
+	    try:
+                if self.waitForNotifications(self.WAIT_TIME):
+		    if self.featherState != self.delegate.d:
+			self.featherState = self.delegate.d
+        	        map(self.broadcast, peripherals.values())  # This is where failure occurs
+	    except BaseException, e:
+		print "BaseException caught: " + e.message	# This is most commonly caught error
+		self.connected = False
+ 	    except BTLEException, e:
+		print "BTLEException caught from peripheral " + self.addr
+		print BTLEException.message
+		if str(e) == 'Device disconnected':
+		    print self.addr + " disconnected"
+		    self.connected = False
+                    # We don't want to call waitForNotifications and fail too often
+                    time.sleep(self.EXCEPTION_WAIT_TIME)
+		else:
+		    raise
+	    except Exception:
+		print "Caught unknown exception from peripheral " + self.addr
+		print Exception.message
+		self.connected = False
+		break
+
+    def broadcast(self, p):
+        if(self.addr == p.addr):
+            return
         with lock:
-            peripheral = peripherals[self.peripheral_addr]
+            txh = peripherals[p.addr].getCharacteristics(uuid=self.txUUID)[0]
         try:
-            m = MyDelegate(self.peripheral_addr)
-            peripheral.setDelegate(m)
-            print " Configuring RX to notify me on change"
-            peripheral.writeCharacteristic(35, b"\x01\x00", withResponse=True)
-            print " Subscribed..."
+	    txh.write(self.featherState, True) # Note, this succeeds
         except BTLEException:
             print BTLEException.message
-        except BaseException:
-            print "BaseException caught in Thread"
-            print BaseException.message
-
-        while True:
-	    try:
-                if peripheral.waitForNotifications(1):
-                    continue
- 	    except BTLEException:
-		print "BTLEException caught from peripheral " + peripheral.addr
-		print BTLEException.message
-		peripheral.disconnect()
-		break
-	    except BaseException:
-		print "BaseException acught from peripheral " + peripheral.addr
-		print BaseException.message
-		peripheral.disconnect()
-		break
-	    except Exception:
-		print "Caught unknown exception from peripheral " + peripheral.addr
-		print Exception.message
-		peripheral.disconnect()
-		break
 
 
 _devicesToFind = "Adafruit Bluefruit LE"
@@ -88,19 +99,9 @@ while True:
 
             for (adtype, desc, value) in d.getScanData():
                 if (_devicesToFind in value):
-                    try:
-                        p = Peripheral(d.addr, "random")
-                        print " Created Peripheral object for device: " + d.addr
-                        print " Appending " + d.addr + " to list of connected devices"
-                    except BTLEException:
-                        print BTLEException.message
-                        break
-                    except Exception:
-                        print "Unknown Exception"
-                        print Exception.message
-                    with lock:
-                        peripherals[d.addr] = p
                     t = BleThread(d.addr)
+		    with lock:
+		        peripherals[d.addr] = t
                     t.start()
         except:
             print "Unknown error"
