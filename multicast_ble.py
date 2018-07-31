e66a0b68d39254604e5ee0876ec8e82e887d1776
John Damask <johndamask@Johns-MBP.hsd1.ma.comcast.net>
2018-07-31 13:34:07 -0400

    dumb

--- multicast_ble.py
+++ multicast_ble.py
@@ -55,7 +55,7 @@ class MyDelegate(DefaultDelegate):
         json_payload = '{"state":{"desired":{"property":"' + binascii.b2a_hex(self.d) + '"}}}'
        #print(json_payload)
        # shadow.shadowUpdate(json_payload, self.customShadowCallback_Update, 5)
-        shadow.shadowUpate(json_payload)
+        shadow.shadowUpdate(json_payload)
         shadow.publish(json_payload)
 
     # # AWS IoT - Custom Shadow callback
